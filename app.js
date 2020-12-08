160b6af0c47ef03a9a3153ca6b3e42ad44ec9c9c
Ahmed M2 Elgaied <46943991+ahmedelg@users.noreply.github.com>
2020-12-08 04:02:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ const http =require('http');
 ////////////////////////////////////////////////
 const shopServer =http.createServer((req, res) => { 
   console.log('shop server has been launched...') 
+  //   process.exit()
 })
 shopServer.listen(3000);
